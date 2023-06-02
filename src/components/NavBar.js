5c53677e88765a8ad51417271043890e8140ac8f
Ryan Rizeq <ryanrizeq@gmail.com>
2023-06-02 15:35:30 -0700

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,9 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{links.map((title) => {
+    return <a key={title} href={`#${title}`}>{title}</a>
+  })}</nav>;
 }
 
 export default NavBar;
